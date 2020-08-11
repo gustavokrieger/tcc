9e27e28b491fc742b8608628abcb6f33964064f9
gusta <gustavopcassol@gmail.com>
2020-08-10 23:18:51 -0300

    adição de envio de imagem para backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,60 @@
-import React from 'react';
-import {Button} from '@material-ui/core';
-import {UploadButton} from './UploadButton';
+import React from "react";
+import {UploadButton} from "./UploadButton";
+import assert from "assert";
 
 export class App extends React.Component {
 
-    handleClick() {
-        console.log("alo")
+    // todo tentar remover construtor
+    constructor(props: Readonly<{}>) {
+        super(props);
+
+        this.handleUploadButtonChange = this.handleUploadButtonChange.bind(this);
+    }
+
+    async handleUploadButtonChange(event: React.ChangeEvent<HTMLInputElement>) {
+        assert(event.target.files !== null)
+
+        const file = event.target.files[0];
+
+        const formData = new FormData();
+
+        formData.append(
+            "myFile",
+            file,
+            file.name
+        );
+
+        const input = "http://localhost:8080/tcc_backend_war_exploded/TestServlet";
+
+        const init = {
+            method: "POST",
+            body: formData,
+            // todo ver ser precisa de header
+            // headers: {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+        }
+
+        const response = await fetch(input, init);
+
+        assert(response.ok)
+
+        // todo continuar
+        // if (response.body !== null) {
+        //     // body is ReadableStream<Uint8Array>
+        //     // parse as needed, e.g. reading directly, or
+        //     const asString = new TextDecoder("utf-8").decode(response.body);
+        //     // and further:
+        //     const asJSON = JSON.parse(asString);  // implicitly 'any', make sure to verify type on runtime.
+        // }
+
     }
 
     render() {
         return (
             <div className="app">
-                <UploadButton/>
-                <FileUpload
-                    value={1}
-                    onClick={() => this.handleClick()}
+                <UploadButton
+                    onChange={this.handleUploadButtonChange}
                 />
             </div>
         );
     }
-}
-
-type FileUploadProps = {
-    value: number,
-    onClick: () => void
-}
-
-function FileUpload(props: FileUploadProps) {
-    return (
-        <Button className="file-upload" variant="contained" onClick={props.onClick}>
-            {props.value}
-        </Button>
-    );
 }
--- frontend/src/UploadButton.tsx
+++ frontend/src/UploadButton.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            '& > *': {
+            "& > *": {
                 margin: theme.spacing(1),
             },
         },
         input: {
-            display: 'none',
+            display: "none",
         },
     }),
 );
 
-export function UploadButton() {
+type UploadButtonProps = {
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+export function UploadButton(props: UploadButtonProps) {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
             <input
-                accept="image/*"
                 className={classes.input}
                 id="contained-button-file"
+                accept="image/*"
                 multiple
                 type="file"
+                onChange={props.onChange}
             />
             <label htmlFor="contained-button-file">
                 <Button variant="contained" color="primary" component="span">
