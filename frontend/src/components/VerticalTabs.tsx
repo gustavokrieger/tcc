19c16722f4e8e37ab10aabf86d668d9ecb53fa91
gusta <gustavopcassol@gmail.com>
2020-08-17 13:17:13 -0300

    alteração para componente utilizar array ao invés de literal

--- frontend/src/components/VerticalTabs.tsx
+++ frontend/src/components/VerticalTabs.tsx
@@ -58,6 +58,38 @@ export default function VerticalTabs() {
     setValue(newValue);
   };
 
+  // todo trocar para ser por props
+  const elements = [
+    {
+      label: 'zero',
+      values: ['zero_value', 'one_value'],
+    },
+    {
+      label: 'one',
+      values: ['two_value', 'three_value'],
+    },
+  ];
+
+  const labels = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    const label = elements[i].label;
+    labels.push(<Tab label={label} {...a11yProps(i)} />);
+  }
+
+  const tabPanelValues = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    const values = elements[i].values;
+    for (const entry of values) {
+      tabPanelValues.push(
+        <TabPanel value={value} index={i}>
+          {entry}
+        </TabPanel>
+      );
+    }
+  }
+
   return (
     <div className={classes.root}>
       <Tabs
@@ -68,35 +100,9 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         className={classes.tabs}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
+        {labels}
       </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+      {tabPanelValues}
     </div>
   );
 }
