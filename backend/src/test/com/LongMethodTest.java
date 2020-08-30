07efc142abc1f989ebfebd3082b2302ffaa993eb
gusta <gustavopcassol@gmail.com>
2020-08-29 23:30:36 -0300

    adição de testes para LongMethod e LongParameterList

---
+++ backend/src/test/com/LongMethodTest.java
@@ -0,0 +1,37 @@
+package test.com;
+
+class LongMethodTest implements PmdTest {
+
+  public String getInputCode() {
+    return "package com;\n"
+        + "\n"
+        + "class Example {\n"
+        + "\n"
+        + "    void test(){\n"
+        + "        System.out.println(\"\");\n".repeat(100)
+        + "    }\n"
+        + "\n"
+        + "}\n";
+  }
+
+  public String getExpectedResult() {
+    return "{\"formatVersion\":0,"
+        + "\"pmdVersion\":\"6.25.0\","
+        + "\"timestamp\":\"\","
+        + "\"files\":["
+        + "{\"filename\":\"\","
+        + "\"violations\":["
+        + "{\"beginline\":5,"
+        + "\"begincolumn\":5,"
+        + "\"endline\":106,"
+        + "\"endcolumn\":5,"
+        + "\"description\":\"Avoid really long methods.\","
+        + "\"rule\":\"ExcessiveMethodLength\","
+        + "\"ruleset\":\"Design\","
+        + "\"priority\":3,"
+        + "\"externalInfoUrl\":\"https://pmd.github.io/pmd-6.25.0/pmd_rules_java_design.html#excessivemethodlength\"}]}],"
+        + "\"suppressedViolations\":[],"
+        + "\"processingErrors\":[],"
+        + "\"configurationErrors\":[]}";
+  }
+}
---
+++ backend/src/test/com/LongParameterListTest.java
@@ -0,0 +1,39 @@
+package test.com;
+
+class LongParameterListTest implements PmdTest {
+
+  public String getInputCode() {
+    return "package com;\n"
+        + "\n"
+        + "public class ExampleLongParameter {\n"
+        + "\n"
+        + "  public void addPerson(\n"
+        + "      int birthYear, int birthMonth, int birthDate, int height, int weight, int a, int b, int c,\n"
+        + "      int d, int e) {\n"
+        + "    return;\n"
+        + "  }\n"
+        + "\n"
+        + "}";
+  }
+
+  public String getExpectedResult() {
+    return "{\"formatVersion\":0,"
+        + "\"pmdVersion\":\"6.25.0\","
+        + "\"timestamp\":\"\","
+        + "\"files\":["
+        + "{\"filename\":\"\","
+        + "\"violations\":["
+        + "{\"beginline\":5,"
+        + "\"begincolumn\":24,"
+        + "\"endline\":7,"
+        + "\"endcolumn\":19,"
+        + "\"description\":\"Avoid long parameter lists.\","
+        + "\"rule\":\"ExcessiveParameterList\","
+        + "\"ruleset\":\"Design\","
+        + "\"priority\":3,"
+        + "\"externalInfoUrl\":\"https://pmd.github.io/pmd-6.25.0/pmd_rules_java_design.html#excessiveparameterlist\"}]}],"
+        + "\"suppressedViolations\":[],"
+        + "\"processingErrors\":[],"
+        + "\"configurationErrors\":[]}";
+  }
+}
