f5ffa35eacbc6c5e2638beeb9e820d4d7d9b6b6e
gusta <gustavopcassol@gmail.com>
2020-09-15 00:56:03 -0300

    melhoria na descrição do método de LongMethod

--- frontend/src/code_smells_descriptions/LongMethodDescription.ts
+++ frontend/src/code_smells_descriptions/LongMethodDescription.ts
@@ -1,7 +1,16 @@
 import CodeSmellDescription from './CodeSmellDescription';
+import assert from 'assert';
 
 export default class LongMethodDescription extends CodeSmellDescription {
   getDescription(): string {
-    return 'Descrição de método longo';
+    const methodName = this.getMethodName();
+    return `O método "${methodName}" possui linhas demais.`;
+  }
+
+  private getMethodName(): string {
+    const regex = /(?:[a-z]+) ([\w$]+)\(/; // todo refatorar para ser classe reutilizavel
+    const match = this.codeThatCausedViolation.match(regex);
+    assert(match !== null);
+    return match[1];
   }
 }
