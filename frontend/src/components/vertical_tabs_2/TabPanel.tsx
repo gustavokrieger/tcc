bf910316cf692566f76cb3715f421e676e2a6941
gusta <gustavopcassol@gmail.com>
2020-10-02 17:16:03 -0300

    VerticalTabs2 foi tornado dinamico

--- frontend/src/components/vertical_tabs_2/TabPanel.tsx
+++ frontend/src/components/vertical_tabs_2/TabPanel.tsx
@@ -9,19 +9,16 @@ interface Props {
 }
 
 export default function TabPanel(props: Props) {
-  const {children, value, index, ...other} = props;
-
   return (
     <div
       role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
+      hidden={props.value !== props.index}
+      id={`vertical-tabpanel-${props.index}`}
+      aria-labelledby={`vertical-tab-${props.index}`}
     >
-      {value === index && (
+      {props.value === props.index && (
         <Box p={3}>
-          <Typography>{children}</Typography>
+          <Typography>{props.children}</Typography>
         </Box>
       )}
     </div>
