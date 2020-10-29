8297c60a4d7c96d3531e49f1d7b7d9a1b8c3de98
gusta <gustavopcassol@gmail.com>
2020-10-28 23:17:21 -0300

    adição teste de case com default para SwitchStatements

---
+++ backend/src/test/java/br/univali/tccbackend/pmd/SwitchStatements/CaseWithDefaultTest.java
@@ -0,0 +1,44 @@
+package br.univali.tccbackend.pmd.SwitchStatements;
+
+import br.univali.tccbackend.pmd.PmdTest;
+
+class CaseWithDefaultTest extends PmdTest {
+
+  @Override
+  protected String getInputCode() {
+    return "public class Foo {\n"
+        + "  public void bar(int x) {\n"
+        + "    switch (x) {\n"
+        + "      case 1: {\n"
+        + "        System.out.println(\"\");\n".repeat(3)
+        + "        break;\n"
+        + "      } default: {\n"
+        + "        System.out.println(\"\");\n".repeat(4)
+        + "      }\n"
+        + "    }\n"
+        + "  }\n"
+        + "}";
+  }
+
+  @Override
+  protected String getExpectedResult() {
+    return "{\"formatVersion\":0,"
+        + "\"pmdVersion\":\"" + PMD_VERSION + "\","
+        + "\"timestamp\":\"\","
+        + "\"files\":["
+        + "{\"filename\":\"\","
+        + "\"violations\":["
+        + "{\"beginline\":3,"
+        + "\"begincolumn\":5,"
+        + "\"endline\":15,"
+        + "\"endcolumn\":5,"
+        + "\"description\":\"A high ratio of statements to labels in a switch statement.  Consider refactoring.\","
+        + "\"rule\":\"SwitchDensity\","
+        + "\"ruleset\":\"Design\","
+        + "\"priority\":3,"
+        + "\"externalInfoUrl\":\"" + PMD_RULES_URL + "#switchdensity\"}]}],"
+        + "\"suppressedViolations\":[],"
+        + "\"processingErrors\":[],"
+        + "\"configurationErrors\":[]}";
+  }
+}
