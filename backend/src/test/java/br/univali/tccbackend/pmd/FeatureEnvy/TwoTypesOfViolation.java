c8c72e25b5fb6c2917cf4f18e2de901f028b317e
gusta <gustavopcassol@gmail.com>
2020-11-04 12:20:36 -0300

    adição de teste

--- backend/src/test/java/br/univali/tccbackend/pmd/FeatureEnvy/TwoTypesOfViolation.java
+++ backend/src/test/java/br/univali/tccbackend/pmd/FeatureEnvy/TwoTypesOfViolation.java
@@ -1,27 +1,18 @@
-package br.univali.tccbackend.pmd;
+package br.univali.tccbackend.pmd.FeatureEnvy;
 
-class FeatureEnvyTest extends PmdTest {
+import br.univali.tccbackend.pmd.PmdTest;
+
+class TwoTypesOfViolation extends PmdTest {
 
   @Override
   protected String getInputCode() {
     return "public class Foo {\n"
-        + "    /**\n"
-        + "     * This example will result in two violations.\n"
-        + "     */\n"
+        + "\n"
         + "    public void example(Bar b) {\n"
-        + "        // this method call is ok, as b is a parameter of \"example\"\n"
         + "        C c = b.getC();\n"
-        + "\n"
-        + "        // this method call is a violation, as we are using c, which we got from B.\n"
-        + "        // We should ask b directly instead, e.g. \"b.doItOnC();\"\n"
         + "        c.doIt();\n"
-        + "\n"
-        + "        // this is also a violation, just expressed differently as a method chain without temporary variables.\n"
         + "        b.getC().doIt();\n"
-        + "\n"
-        + "        // a constructor call, not a method call.\n"
         + "        D d = new D();\n"
-        + "        // this method call is ok, because we have create the new instance of D locally.\n"
         + "        d.doSomethingElse();\n"
         + "    }\n"
         + "}";
@@ -35,18 +26,18 @@ class FeatureEnvyTest extends PmdTest {
         + "\"files\":["
         + "{\"filename\":\"\","
         + "\"violations\":["
-        + "{\"beginline\":11,"
+        + "{\"beginline\":5,"
         + "\"begincolumn\":9,"
-        + "\"endline\":11,"
+        + "\"endline\":5,"
         + "\"endcolumn\":16,"
         + "\"description\":\"Potential violation of Law of Demeter (object not created locally)\","
         + "\"rule\":\"LawOfDemeter\","
         + "\"ruleset\":\"Design\","
         + "\"priority\":3,"
         + "\"externalInfoUrl\":\"" + PMD_RULES_URL + "#lawofdemeter\"},"
-        + "{\"beginline\":14,"
+        + "{\"beginline\":6,"
         + "\"begincolumn\":9,"
-        + "\"endline\":14,"
+        + "\"endline\":6,"
         + "\"endcolumn\":23,"
         + "\"description\":\"Potential violation of Law of Demeter (method chain calls)\","
         + "\"rule\":\"LawOfDemeter\","
