eed8e4acacead912e9f869d48e971fa839d5e74d
gusta <gustavopcassol@gmail.com>
2020-11-10 01:10:30 -0300

    melhoria de descrições

--- frontend/src/code_smells/implementations/LongMethod.ts
+++ frontend/src/code_smells/implementations/LongMethod.ts
@@ -18,7 +18,7 @@ export default class LongMethod implements CodeSmell {
     return (
       `O método "${methodName}" é um Método Longo pois ele possui linhas em excesso. São ${lines} linhas, enquanto ` +
       'o ideal seria um número menor do que 50. ' +
-      'O problema desse tipo de método, é que o seu tamanho excessivo pode acabar tornando difícil entender ' +
+      'O problema desse tipo de método é que o seu tamanho excessivo pode acabar tornando difícil entender ' +
       'o que ele faz, além de tornar o seu código menos reutilizável (métodos pequenos são mais versáteis).' +
       `A solução pode ser identificar trechos de código do método "${methodName}" que possam ser extraídos, ` +
       'mover esses trechos para novos métodos e chamar os novos métodos no lugar dos trechos extraídos.'
