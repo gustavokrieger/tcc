9fc5a83ecd0e877042411dbf478ecfb03d48e122
gusta <gustavopcassol@gmail.com>
2020-11-23 21:00:57 -0300

    adição de mock nos testes para corrigir erro de execução

--- backend/src/test/java/br/univali/tccbackend/pmd/PmdTest.java
+++ backend/src/test/java/br/univali/tccbackend/pmd/PmdTest.java
@@ -1,12 +1,15 @@
 package br.univali.tccbackend.pmd;
 
+import br.univali.tccbackend.ContextValues;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.IOException;
+import java.nio.file.Path;
 import javax.naming.NamingException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public abstract class PmdTest {
 
@@ -25,10 +28,18 @@ public abstract class PmdTest {
   protected abstract String getInputCode();
 
   private PmdAnalysisResult getPmdAnalysisResult(String code) throws IOException, NamingException {
-    PmdRunner pmdRunner = new TextPmdRunner(code);
+    ContextValues contextValues = getMockedContextValues();
+    PmdRunner pmdRunner = new TextPmdRunner(code, contextValues);
     PmdAnalysisResult pmdAnalysisResult = pmdRunner.run();
     removeDataThatVaries(pmdAnalysisResult);
     return pmdAnalysisResult;
+  }
+
+  private ContextValues getMockedContextValues() throws NamingException {
+    ContextValues contextValues = Mockito.mock(ContextValues.class);
+    Path rulesetFile = Path.of("pmd-ruleset.xml").toAbsolutePath();
+    Mockito.when(contextValues.lookupPmdRulesetPath()).thenReturn(rulesetFile.toString());
+    return contextValues;
   }
 
   private void removeDataThatVaries(PmdAnalysisResult pmdAnalysisResult) {
